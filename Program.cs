1dc4b0c2651207a00675b8542f10bd822d8fa444
@kibblewhite <kibblewhite@live.com>
2021-11-19 23:09:14 +0100

    Working sample of connecting and retrieving data from both gRPC services identity-svc and solutions-fulfilment-svc

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using ECS.DTO.v1.Identity.Request;
+using ECS.DTO.v1.SolutionsFulfilment.Request;
+using ECS.Services.v1.Identity;
+using ECS.Services.v1.SolutionsFulfilment;
+using Grpc.Net.Client;
+using System.Text.Json;
+
+
+var opt = new JsonSerializerOptions() { WriteIndented = true };
+
+var http_handler_01 = new HttpClientHandler
+{
+    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+};
+
+using var channel_01 = GrpcChannel.ForAddress("https://solutions-fulfilment-svc:32443", new GrpcChannelOptions { HttpHandler = http_handler_01 });
+
+var client_01 = new ClientPlan.ClientPlanClient(channel_01);
+
+var request_01 = new ListAllClientPlansRequest();
+
+var response_01 = await client_01.ListAllClientPlansAsync(request_01);
+
+var data_01 = response_01.Result.Data;
+
+foreach (var data in data_01)
+{
+    string data_str_json = JsonSerializer.Serialize(data, opt);
+    Console.WriteLine(data_str_json);
+}
+
+Console.WriteLine(" --------[ next service request ]-------- ");
+
+var http_handler_02 = new HttpClientHandler
+{
+    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+};
+
+using var channel_02 = GrpcChannel.ForAddress("https://identity-svc:31443", new GrpcChannelOptions { HttpHandler = http_handler_02 });
+
+var client_02 = new Greeter.GreeterClient(channel_02);
+
+var request_02 = new HelloRequest();
+
+var response_02 = await client_02.SayHelloAsync(request_02);
+
+var data_02 = response_02.Result.Data;
+
+foreach (var data in data_02)
+{
+    string data_str_json = JsonSerializer.Serialize(data, opt);
+    Console.WriteLine(data_str_json);
+}
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # ecs-protobuf-csharp-01
+
+
+The nuget package source needs to be configured within the Visual Studio IDE
+
+Name: protobuf-csharp
+
+Source: https://gitlab.com/api/v4/projects/31452783/packages/nuget/index.json
+
+
+> Both the identity-svc and solutions-fulfilment-svc gRPC services should be running locally on the development machine.
+
+Returned Results should look roughly the following (last updated 2021-11-19):
+
+```
+{
+  "PricingModelGuid": "00000000-0000-0000-0000-000000000000",
+  "Title": "Testing...",
+  "Synopsis": "Some testing text...",
+  "HtmlContent": "\u003Cdiv\u003EHeylo World\u003C/div\u003E",
+  "Iso4217Code": "EUR",
+  "SalesValue": 2345
+}
+ --------[ next service request ]--------
+{
+  "Message": "Heylo World 01 [start]"
+}
+{
+  "Message": "Heylo World 02"
+}
+{
+  "Message": "Heylo World 03 [ end ]"
+}
+{
+  "Message": "Default"
+}
+
+```
