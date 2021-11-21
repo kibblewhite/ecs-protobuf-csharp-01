b7a0c83a607e25d8a31cea914e818220889a3f1d
@kibblewhite <kibblewhite@live.com>
2021-11-21 14:48:37 +0100

    Minor spacing updates and syntax changes to be closer to C# 10

--- Program.cs
+++ Program.cs
@@ -1,111 +1,116 @@
-﻿using ECS.DTO.v1.Identity.Request;
-using ECS.DTO.v1.SolutionsFulfilment.Request;
-using ECS.DTO.v1.SolutionsFulfilment.Response;
-using ECS.Services.v1.Identity;
-using ECS.Services.v1.SolutionsFulfilment;
-using Grpc.Core;
-using Grpc.Net.Client;
-using System.Security.Cryptography.X509Certificates;
-using System.Text.Json;
+﻿namespace ecs_protobuf_csharp_01;
 
-
-var opt = new JsonSerializerOptions() { WriteIndented = true };
-
-Console.WriteLine(" --------[ first service request ]-------- ");
-
-HttpClientHandler http_handler_01 = new()
+public class Program
 {
-    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-};
 
-using GrpcChannel channel_01 = GrpcChannel.ForAddress("https://solutions-fulfilment-svc:32443", new GrpcChannelOptions { HttpHandler = http_handler_01 });
-
-ClientPlan.ClientPlanClient client_01 = new(channel_01);
-
-ListAllClientPlansRequest request_01 = new();
-
-try
-{
-    var response_01 = await client_01.ListAllClientPlansAsync(request_01);
-    var data_01 = response_01.Result.Data;
-    foreach (var data in data_01)
+    public static void Main(string[] args)
     {
-        string data_str_json = JsonSerializer.Serialize(data, opt);
-        Console.WriteLine(data_str_json);
+        MainAsync(args).GetAwaiter().GetResult();
     }
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
 
-Console.WriteLine(" --------[ next service request ]-------- ");
-
-HttpClientHandler http_handler_02 = new()
-{
-    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-};
-
-using GrpcChannel channel_02 = GrpcChannel.ForAddress("https://identity-svc:31443", new GrpcChannelOptions { HttpHandler = http_handler_02 });
-
-Greeter.GreeterClient client_02 = new(channel_02);
-
-HelloRequest request_02 = new();
-
-try
-{
-    var response_02 = await client_02.SayHelloAsync(request_02);
-    var data_02 = response_02.Result.Data;
-    foreach (var data in data_02)
+    public static async Task MainAsync(string[] args)
     {
-        string data_str_json = JsonSerializer.Serialize(data, opt);
-        Console.WriteLine(data_str_json);
+
+        var opt = new JsonSerializerOptions() { WriteIndented = true };
+
+        Console.WriteLine(" --------[ first service request ]-------- ");
+
+        HttpClientHandler http_handler_01 = new()
+        {
+            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        };
+
+        using GrpcChannel channel_01 = GrpcChannel.ForAddress("https://solutions-fulfilment-svc:32443", new GrpcChannelOptions { HttpHandler = http_handler_01 });
+
+        ClientPlan.ClientPlanClient client_01 = new(channel_01);
+
+        ListAllClientPlansRequest request_01 = new();
+
+        try
+        {
+            var response_01 = await client_01.ListAllClientPlansAsync(request_01);
+            var data_01 = response_01.Result.Data;
+            foreach (var data in data_01)
+            {
+                string data_str_json = JsonSerializer.Serialize(data, opt);
+                Console.WriteLine(data_str_json);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.WriteLine(" --------[ next service request ]-------- ");
+
+        HttpClientHandler http_handler_02 = new()
+        {
+            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        };
+
+        using GrpcChannel channel_02 = GrpcChannel.ForAddress("https://identity-svc:31443", new GrpcChannelOptions { HttpHandler = http_handler_02 });
+
+        Greeter.GreeterClient client_02 = new(channel_02);
+
+        HelloRequest request_02 = new();
+
+        try
+        {
+            var response_02 = await client_02.SayHelloAsync(request_02);
+            var data_02 = response_02.Result.Data;
+            foreach (var data in data_02)
+            {
+                string data_str_json = JsonSerializer.Serialize(data, opt);
+                Console.WriteLine(data_str_json);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.WriteLine(" --------[ last service request with certification authentication ]-------- ");
+
+        X509Certificate2? x509_certificate = new("Infrastructure\\pass_cert.p12", "development-password");
+        ArgumentNullException.ThrowIfNull(x509_certificate, nameof(x509_certificate));
+        string certificate_raw_data = x509_certificate.GetRawCertDataString();
+
+        CallOptions call_options = new(new Metadata
+        {   // note (2021-11-21|kibble): this is for the service certificate authentication / when the client requires access to particular services when a client is without a JWT
+            new Metadata.Entry("Authorization", $"Certificate {certificate_raw_data}")
+        });
+
+        HttpClientHandler http_handler_03 = new()
+        {
+            // todo (2021-11-21|kibble): Rather that dismiss any dangerous server ssl certificates, make sure to include the real certificate in the future and lock this down security wise!
+            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        };
+
+        using GrpcChannel channel_03 = GrpcChannel.ForAddress("https://solutions-fulfilment-svc:32443", new GrpcChannelOptions { HttpHandler = http_handler_02 });
+        ArgumentNullException.ThrowIfNull(channel_03, nameof(channel_03));
+
+        PriceModel.PriceModelClient? client_03 = new(channel_03);
+        ArgumentNullException.ThrowIfNull(client_03, nameof(client_03));
+
+        try
+        {
+            ListAllPriceModelsRequest request_03 = new();
+            ListAllPriceModelsResponse? response_03 = await client_03.ListAllPriceModelsAsync(request_03, call_options);
+            ArgumentNullException.ThrowIfNull(response_03, nameof(response_03));
+
+            var data_03 = response_03.Result.Data;
+            foreach (var data in data_03)
+            {
+                string data_str_json = JsonSerializer.Serialize(data, opt);
+                Console.WriteLine(data_str_json);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.ReadLine();
     }
+
 }
-catch(Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
-
-Console.WriteLine(" --------[ last service request with certification authentication ]-------- ");
-
-X509Certificate2? x509_certificate = new("Infrastructure\\pass_cert.p12", "development-password");
-ArgumentNullException.ThrowIfNull(x509_certificate, nameof(x509_certificate));
-string certificate_raw_data = x509_certificate.GetRawCertDataString();
-
-CallOptions call_options = new(new Metadata
-{   // note (2021-11-21|kibble): this is for the service certificate authentication / when the client requires access to particular services when a client is without a JWT
-    new Metadata.Entry("Authorization", $"Certificate {certificate_raw_data}")
-});
-
-HttpClientHandler http_handler_03 = new()
-{
-    // todo (2021-11-21|kibble): Rather that dismiss any dangerous server ssl certificates, make sure to include the real certificate in the future and lock this down security wise!
-    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-};
-
-using GrpcChannel channel_03 = GrpcChannel.ForAddress("https://solutions-fulfilment-svc:32443", new GrpcChannelOptions { HttpHandler = http_handler_02 });
-ArgumentNullException.ThrowIfNull(channel_03, nameof(channel_03));
-
-PriceModel.PriceModelClient? client_03 = new(channel_03);
-ArgumentNullException.ThrowIfNull(client_03, nameof(client_03));
-
-try
-{
-    ListAllPriceModelsRequest request_03 = new();
-    ListAllPriceModelsResponse? response_03 = await client_03.ListAllPriceModelsAsync(request_03, call_options);
-    ArgumentNullException.ThrowIfNull(response_03, nameof(response_03));
-
-    var data_03 = response_03.Result.Data;
-    foreach (var data in data_03)
-    {
-        string data_str_json = JsonSerializer.Serialize(data, opt);
-        Console.WriteLine(data_str_json);
-    }
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
-
-Console.ReadLine();
---
+++ Usings.cs
@@ -0,0 +1,10 @@
+﻿global using ECS.DTO.v1.Identity.Request;
+global using ECS.DTO.v1.SolutionsFulfilment.Request;
+global using ECS.DTO.v1.SolutionsFulfilment.Response;
+global using ECS.Services.v1.Identity;
+global using ECS.Services.v1.SolutionsFulfilment;
+global using Grpc.Core;
+global using Grpc.Net.Client;
+global using System.Security.Cryptography.X509Certificates;
+global using System.Text.Json;
+
